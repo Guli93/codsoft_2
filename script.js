dee281b5395bf979bd72b1148e7317f3ed660bc3
Gulista <gulistasiddiqui5@gmail.com>
2023-08-12 22:27:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// console.log("check");
+let pr1=document.getElementById("pr1");
+let pr2=document.getElementById("pr2");
+let latestBtn=document.getElementById("latest-btn");
+latestBtn.addEventListener('click',function(){
+    console.log("check");
+    pr1.style.display="none";
+    pr2.style.display="none";
+})
